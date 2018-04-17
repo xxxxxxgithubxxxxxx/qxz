b14b405d6b0167bfdc8b90de06b568a1e0b9f51d
chengaiguo <2276129351@qq.com>
2018-04-17 18:01:52 +0800

    aiguo_all获取数据

--- src/components/aiguo/aiguo_all.vue
+++ src/components/aiguo/aiguo_all.vue
@@ -1,17 +1,30 @@
 	<template>
 		<div>
-			全部页面
+			<ul>
+				<li>狂魔车队</li>
+				<li><h3>国产GPS码表</h3></li>
+				<li>个问佛为了建一我佛微问佛为了建一我佛问佛为了建一我佛积分</li>
+				<li><img />图片</li>
+			</ul>
 		</div>
 	</template>
 	<script>
+	import axios from 'axios';
 	export default{
 		data(){
 			return{
-				
+				datalist:[111,222,333]
 			}
 		},
 		computed:{
 
+		},
+		mounted(){
+			axios.get("/api/v4/new_get_topics?start=0&count=10&channel_id=0").then(res=>{
+				console.log(res);
+			}).catch(function(err){
+				console.log(err);
+			})
 		}
 	}
 	</script>
