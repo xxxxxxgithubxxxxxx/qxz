6ea4c3415eadb59a109785a1ce98cb375c17e4ae
xianghaitao <1261445435@qq.com>
2018-04-20 16:14:17 +0800

    所有提交

---
+++ src/components/Contact_us.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<h3>联系我们</h3>
+		<p>用户交流QQ群</p>
+		<ul>
+			<li>
+				<img src="" alt="" />
+				<p>安卓用户交流群</p>
+			</li>
+			<li>
+				<img src="" alt="" />
+				<p>iOS用户交流群</p>
+			</li>
+			<li>
+				<img src="" alt="" />
+				<p>全国用户交流群</p>
+			</li>
+		</ul>
+		<p>行者官网QQ</p>
+		<p>2093696425</p>
+		<p>联系电话</p>
+		<p>021-32600166</p>
+		<p>官方微博</p>
+		<p>行者软件</p>
+		<p>微信公众号</p>
+		<p>微信搜索：imxingzhe</p>
+		<img src="" alt="" />
+		<p>联系我们及商务合作：contact@imxingzhe.com</p>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
