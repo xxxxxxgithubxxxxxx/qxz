9731ebaa97a9cc97c7238eb131be89bcb3d7f334
xianghaitao <1261445435@qq.com>
2018-04-20 18:07:20 +0800

    over

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,18 +1,18 @@
 <template>
-	<div>
+	<div id="box">
 		<h3>联系我们</h3>
 		<p>用户交流QQ群</p>
 		<ul>
 			<li>
-				<img src="" alt="" />
+				<img src="http://www.imxingzhe.com/static/v3/contact/android-new.png" alt="" />
 				<p>安卓用户交流群</p>
 			</li>
 			<li>
-				<img src="" alt="" />
+				<img src="http://www.imxingzhe.com/static/v3/contact/ios-test.png" alt="" />
 				<p>iOS用户交流群</p>
 			</li>
 			<li>
-				<img src="" alt="" />
+				<img src="http://www.imxingzhe.com/static/v3/contact/all.png" alt="" />
 				<p>全国用户交流群</p>
 			</li>
 		</ul>
@@ -23,14 +23,69 @@
 		<p>官方微博</p>
 		<p>行者软件</p>
 		<p>微信公众号</p>
-		<p>微信搜索：imxingzhe</p>
-		<img src="" alt="" />
-		<p>联系我们及商务合作：contact@imxingzhe.com</p>
+		<p>微信搜索：<span>imxingzhe</span></p>
+		<img src="http://www.imxingzhe.com/static/v3/contact/xingzhe-weixinqrcode.png" alt="" />
+		<p>联系我们及商务合作：<span>contact@imxingzhe.com</span></p>
+		<p>联系我们及商务合作：<span>contact@imxingzhe.com</span></p>
+		<p>联系我们及商务合作：<span>contact@imxingzhe.com</span></p>
+		<div>
+			上海办公地址：上海市杨浦区政学路77号 innospace+ 行者上海中心
+		</div>
+		<div>北京办公地址：北京市朝阳区酒仙桥东路10号冠捷北京西门二层 madnet 行者北京中心</div>
+		<div>杭州办公地址：杭州市西湖区池华街398号丰盛九玺8号楼2101 行者杭州中心</div>
+		<div>深圳办公地址：深圳宝安区新安六路御景苑众里创业社区 行者深圳中心</div>
 	</div>
 </template>
 
 <script>
+	
 </script>
 
-<style>
+<style lang="scss" scoped type="text/css">
+	p,ul,li,span,body{
+		margin: 0;
+		padding: 0;
+		font-size: 0.08rem;
+	}
+	li{
+		list-style: none;
+	}
+	img{
+		display: block;
+		border: none;
+		width: 80%;
+	}
+	h3{
+		font-size: 0.125rem;
+	}
+	#box{
+		padding: 0.04rem 0.04rem;
+
+		ul{
+			overflow:hidden;
+			li{
+				float: left;
+				width: 1.5rem;
+				height:1.5rem;
+				p{
+					text-align: center;
+					line-height: 0.25rem;
+				}
+			}
+		}
+		p{
+			line-height: 0.25rem;
+			span{
+				color: #0086B3;
+			}
+		}
+		p:nth-of-type(2n+3){
+			text-indent: 0.15rem;
+		}
+		div{
+			font-size: 0.08rem;
+			line-height: 0.25rem;
+		}
+	}
+	
 </style>
--- src/components/help.vue
+++ src/components/help.vue
@@ -1,6 +1,43 @@
 <template>
-	<div>
-		help页面
+	<div id="box">
+		<p class="top">帮助中心</p>
+		<div>
+			<ul>
+				<li>
+					<p class="iconfont icon-shouji"></p>
+					<p>APP使用</p>
+				</li>
+				<li>
+					<p class="iconfont icon-genggaishouhuodizhi"></p>
+					<p>路书相关</p>
+				</li>
+				<li>
+					<p class="iconfont icon-iocnshiping"></p>
+					<p>视频介绍</p>
+				</li>
+				<li>
+					<p class="iconfont icon-iconshousuo"></p>
+					<p>积分优惠</p>
+				</li>
+				<li>
+					<p class="iconfont icon-lianxidianyuan"></p>
+					<p>联系我们</p>
+				</li>
+				<li>
+					<p class="iconfont icon-youjian"></p>
+					<p>问题反馈</p>
+				</li>
+			</ul>
+			<p class="wenti">常见问题解答</p>
+			<p>GPS定位常见问题</p>
+			<p>运动首页设置</p>
+			<p>语音播报问题</p>
+			<p>离线地图</p>
+			<p>注册登录问题</p>
+			<p>行者积分问题</p>
+			<p>后台运行异常中断解决办法（安卓）</p>
+			<p class="buttom">没有找到遇到的问题？你可以<span>与我们取得联系</span>。</p>
+		</div>
 	</div>
 </template>
 <script>
@@ -16,7 +53,74 @@ export default{
 }
 </script>
 <style lang="scss" scoped type="text/css">
-	div{
-		background:red;
+	p,ul,li,span,body{
+		margin: 0;
+		padding: 0;
+		font-size: 0.1rem;
+	}
+	li{
+		list-style: none;
+	}
+	img{
+		display: block;
+		border: none;
+		width: 80%;
+	}
+	#box{
+		background: #E9E9E9;
+
+		.top{
+			line-height: 0.3rem;
+			background: #FFFFFF;
+			text-indent: 0.15rem;
+		}
+		div{
+			padding: 0.1rem;
+			ul{
+				overflow:hidden;
+				li{
+					margin: 0.1rem;
+					width: 45%;
+					margin-left: 0.05rem;
+					margin-right: 0.1rem;
+					float: left;
+					background: #FFFFFF;
+					text-align: center;
+					p{
+						font-size: 0.1rem;
+						color: #000000;
+						line-height: 0.2rem;
+					}
+					p:nth-of-type(1){
+						margin-top: 0.3rem;
+						font-size: 0.4rem;
+						line-height: 0.3rem;
+						color: #0000FF;
+					}
+					p:nth-of-type(2){
+						line-height: 0.4rem;
+						margin-bottom: 0.1rem;
+						margin-top: 0.1rem;
+						
+					}
+				}
+			}
+			.wenti{
+				line-height: 0.3rem;
+			}
+			p{
+				font-size: 0.13rem;
+				color: #0000FF;
+				line-height: 0.5rem;
+			}
+			.buttom{
+				margin-top: 1rem;
+				color: #000000;
+				span{
+					font-size: 0.13rem;
+					color: #0000FF;
+				}
+			}
+		}
 	}
 </style>
